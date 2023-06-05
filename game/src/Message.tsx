1e3939a6abc150023986a370b78e2aa273f225cd
Souchen <87101785+SouchenOu@users.noreply.github.com>
2023-06-05 16:42:38 +0100

    Add a component

---
+++ game/src/Message.tsx
@@ -0,0 +1,12 @@
+
+
+// THIS IS COMPONENT
+
+//We should write the name of a function with PASCALCasing
+function Message()
+{
+    // This is javascript XML (JSX)
+    return <h1>Hello souchen</h1>;
+}
+// To use it somewhere we should export it
+export default Message;
