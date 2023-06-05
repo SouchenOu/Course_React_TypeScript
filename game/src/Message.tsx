e583e0b6699ed99f6aa23477f2b27715dd554e11
Souchen <87101785+SouchenOu@users.noreply.github.com>
2023-06-05 20:38:15 +0100

    start working with bootstrap to styling my website

--- game/src/Message.tsx
+++ game/src/Message.tsx
@@ -5,8 +5,11 @@
 //We should write the name of a function with PASCALCasing
 function Message()
 {
+    const name = "souchen";
+    if(name)
+        return <h1>hello {name}</h1>
     // This is javascript XML (JSX)
-    return <h1>Hello souchen</h1>;
+    return <h1>Hello soukaina ouchen</h1>;
 }
 // To use it somewhere we should export it
 export default Message;
