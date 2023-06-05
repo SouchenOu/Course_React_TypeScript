ef990810a51cee606b568a576f93182fea1a3e1c
Souchen <87101785+SouchenOu@users.noreply.github.com>
2023-06-05 21:45:37 +0100

    Use fragment to return multipl element

--- game/src/components/ListGroup.tsx
+++ game/src/components/ListGroup.tsx
@@ -1,13 +1,19 @@
+import {Fragment} from "react" 
+
 function ListGroup()
 {
     return (
-        <ul className="list-group list-group-flush">
-    <li className="list-group-item">An item</li>
-    <li className="list-group-item">A second item</li>
-    <li className="list-group-item">A third item</li>
-    <li className="list-group-item">A fourth item</li>
-    <li className="list-group-item">And a fifth one</li>
-        </ul>
+        //React Fragment is a feature in React that allows you to return multiple elements from a React component
+    <Fragment>
+        <h1>This is the navbar</h1>
+            <ul className="list-group list-group-flush">
+        <li className="list-group-item">An item</li>
+        <li className="list-group-item">A second item</li>
+        <li className="list-group-item">A third item</li>
+        <li className="list-group-item">A fourth item</li>
+        <li className="list-group-item">And a fifth one</li>
+            </ul>
+    </Fragment>
     );
 }
 
