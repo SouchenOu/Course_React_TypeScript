7295e0bd27986bc67b9ae4d1599c4e16c28585fd
Souchen <87101785+SouchenOu@users.noreply.github.com>
2023-06-06 15:28:01 +0100

    using map and console.log

--- game/src/components/ListGroup.tsx
+++ game/src/components/ListGroup.tsx
@@ -3,6 +3,15 @@ import {Fragment} from "react"
 function ListGroup()
 {
     const items = ["New Nork", "San fransisco", "Tokyo", "London"]
+
+    if(items.length == 0)
+        return (
+            //We can write <></> or <Fragment></Fragment>
+            <>
+             <h1>List</h1>
+             <p>No item found</p>
+            </>
+        )
     return (
     /************WE CAN WRITE THIS WITH ANOTHER METHOD */
         //React Fragment is a feature in React that allows you to return multiple elements from a React component
@@ -17,10 +26,14 @@ function ListGroup()
     //         </ul>
     // </Fragment>
     //**************************/
+    
+
+    // We can check if there is items or no outside here
     <Fragment>
         <h1>This is the navbar</h1>
+            {items.length === 0 ? <p>No items</p> : null}
         <ul className="list-group list-group-flush">
-            {items.map((item) => (<li>{item}</li>))}
+            {items.map((item) => (<li className="list-group-item" key = {item}  onClick={() => console.log(item)}>{item}</li>))}
         </ul>
     </Fragment>
     );
