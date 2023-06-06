a3dde7644874991275fc2be4bd37f0ed37d15777
Souchen <87101785+SouchenOu@users.noreply.github.com>
2023-06-06 23:26:33 +0100

    add a small test

--- ts/src/App.tsx
+++ ts/src/App.tsx
@@ -1,10 +1,14 @@
 import Message from './Message';
 import ListGroup from './components/ListGroup'
+import ListGroupTest from './components/ListGroupTest';
 
 function App(){
 
   let elements = ["hello", "souchen", "from 1337"]
-  return <div><ListGroup elements= {elements} heading = "numbers"></ListGroup></div>;
+  return <div><ListGroup elements= {elements} heading = "numbers"></ListGroup>*
+  
+  <ListGroupTest></ListGroupTest>
+  </div>;
 }
 
 export default App;
---
+++ ts/src/components/ListGroupTest.tsx
@@ -0,0 +1,21 @@
+import {Fragment} from "react"
+
+
+function ListGroupTest()
+{
+    return(
+        <Fragment>
+            <h1>test1</h1>
+            <ul>
+                <li>A1</li>
+                <li>A2</li>
+                <li>A3</li>
+            </ul>
+
+        </Fragment>
+    )
+
+}
+
+
+export default ListGroupTest;
