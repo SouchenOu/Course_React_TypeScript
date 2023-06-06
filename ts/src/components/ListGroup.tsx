81f158f9f57dba860ae680cc973073b2f8468756
Souchen <87101785+SouchenOu@users.noreply.github.com>
2023-06-06 22:25:59 +0100

    using hookState

--- ts/src/components/ListGroup.tsx
+++ ts/src/components/ListGroup.tsx
@@ -1,11 +1,12 @@
 import {Fragment} from "react" 
 import { MouseEvent } from "react"
+import {useState} from "react"
 
 function ListGroup()
 {
     const items = ["New Nork", "San fransisco", "Tokyo", "London"]
     const   cities = ["Agadir", "Casablanca", "Tanger", "sala"]
-
+    const  countries = ["Maroc", "Germany", "canada", "Espagne"]
     if(items.length == 0)
         return (
             //We can write <></> or <Fragment></Fragment>
@@ -15,6 +16,10 @@ function ListGroup()
             </>
         )
     const handelClick = (event: MouseEvent) => console.log(event);
+    //let selectedIndex = 0;
+    // HOOK : a hook is a function that allows us to tap into built-in features in react, so this is called the stateHOOK
+    //with stateHook we can tell react that this component can have data or state that will change overtime
+    const [selectedIndex, setSelectedIndex] = useState(-1);
     return (
     /************WE CAN WRITE THIS WITH ANOTHER METHOD */
         //React Fragment is a feature in React that allows you to return multiple elements from a React component
@@ -36,8 +41,9 @@ function ListGroup()
         <h1>This is the navbar</h1>
             {items.length === 0 ? <p>No items</p> : null}
         <ul className="list-group list-group-flush">
-            {items.map((item) => (<li className="list-group-item" key = {item}  onClick={() => console.log(item)}>{item}</li>))}
+            {items.map((item, index) => (<li className={selectedIndex === index ? 'list-group-item active' : 'list-group-item' } key = {item}  onClick={() => console.log(item)}>{item}</li>))}
             {cities.map((city)=> (<li className="list-group-item" key = {city} onClick={handelClick}>{city}</li>))}
+            {countries.map((country, index)=> (<li className={selectedIndex === index ? 'list-group-item active' : 'list-group-item'} key = {country} onClick={() => setSelectedIndex(index)}>{country}</li>))}
         </ul>
     </Fragment>
     );
