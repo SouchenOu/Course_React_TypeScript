884b732c3d72863b7b62981a62ca4d31c271c412
Souchen <87101785+SouchenOu@users.noreply.github.com>
2023-06-07 08:09:58 +0100

    start again

--- ts/src/components/ListGroupTest.tsx
+++ ts/src/components/ListGroupTest.tsx
@@ -3,13 +3,15 @@ import {Fragment} from "react"
 
 function ListGroupTest()
 {
+    let test= ["A1","A2","A3"];
     return(
         <Fragment>
             <h1>test1</h1>
             <ul>
-                <li>A1</li>
+                {/* <li>A1</li>
                 <li>A2</li>
-                <li>A3</li>
+                <li>A3</li> */}
+                {(test.map((t) => (<li className="" onClick={() => console.log("hello")}>{t}</li>)))}
             </ul>
 
         </Fragment>
